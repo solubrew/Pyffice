b63af456446915f671338b9d87a141cb4a5970f1
solubrew <solubrew@solutionsbrewer.com>
2026-04-06 23:46:11 -0400

    reorg active documents and inactive documents to match release plans with nchantdoffice document types

--- pyffice/tags/manager.py
+++ pyffice/tags/manager.py
@@ -21,6 +21,8 @@ import datetime as dt
 # ======================================Solutions Brewer Library Modules==============================================||
 from condor import condor
 from ogma.logma import Logma
+from pyffice.document import PyfficeDocumentManager
+from pyffice.tags.tags import PyfficeTag
 
 # ====================================================================================================================||
 here = join(dirname(__file__), "")  # ||
@@ -28,53 +30,43 @@ log = True
 logma = Logma(__name__)
 
 # ====================================================================================================================||
-pxcfg = join(here, "_data_", "tags.yaml")
+pxcfg = join(here, "_data_", ".yaml")
 
 
-class PyfficeTag(object):
+class PyfficeTagsManager(PyfficeDocumentManager):
     """"""
 
     VERSION = "0.0.1.0.1.0"
 
     def __init__(self, cfg=None):
         """"""
-        self.config = condor.Instruct(pxcfg).select("PyfficeTag").override(cfg)
-        self.description = None
-        self.label = None
-        self.value = None
-        self.doc_type = "tags"
+        super().__init__(cfg)
+        self.config.override(
+            condor.Instruct(pxcfg).select("PyfficeTagsManager")
+        ).override(cfg)
+        self.tags = []
 
-    def load_tag(self, tag=None):
+    def add_tag(self, name, description="", group=None):
         """"""
-        if tag is None:
-            tag = self.config.dikt.get("tag", {})
-        self.set_description(tag.get("description", None))
-        self.set_label(tag.get("label", None))
-        self.set_value(tag.get("value", None))
+        cfg = {"tag": {"name": name, "description": description, "group": group}}
+        tag = PyfficeTag(cfg)
+        tag.load_tag()
+        self.tags.append(tag)
         return self
 
-    def set_description(self, description):
+    def load_document(self, document=None):
         """"""
-        self.description = description
-        return self
-
-    def set_label(self, label):
-        """"""
-        self.label = label
-        return self
-
-    def set_value(self, value):
-        """"""
-        self.value = value
+        logma.info(f"Load Document {document}")
+        if document is None:
+            document = self.config.dikt.get("document", {})
+        super().load_document(document)
+        self.set_tags(document.get("tags", []))
         return self
 
     def to_dict(self):
         """"""
-        doc = {
-            "label": self.label,
-            "description": self.description,
-            "value": self.value,
-        }
+        doc = super().to_dict()
+        doc["document"] = {"tags": self.tags}
         return doc
 
 
