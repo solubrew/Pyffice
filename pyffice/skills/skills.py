5bc17ffba75f30c7aaa36c32e14c9f9d9aa44b65
solubrew <solubrew@solutionsbrewer.com>
2026-05-15 19:56:20 -0400

    fix imports and update dependencies

---
+++ pyffice/skills/skills.py
@@ -0,0 +1,42 @@
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@||
+"""
+---
+<(META)>:
+        docid:
+        name:
+        description: >
+            create a skill system for an internal Ai Agent
+
+
+
+        version: 0.0.0.0.0.0
+        authority: filesystem
+        security: seclvl2
+        <(WT)>: -32
+"""
+
+# -*- coding: utf-8 -*
+# ======================================Standard Library Modules======================================================||
+from os.path import abspath, dirname, join
+import datetime as dt
+
+# ======================================3rd Party Library Modules=====================================================||
+
+# ======================================Solutions Brewer Library Modules==============================================||
+from condor import condor
+from ogma.logma import Logma
+
+# ====================================================================================================================||
+here = join(dirname(__file__), "")  # ||
+log = True
+logma = Logma(__name__)
+
+# ====================================================================================================================||
+pxcfg = join(here, "_data_", ".yaml")
+
+# TODO:
+#   allow to create documents within the database file for the nchantdoffice application
+#   confine to only these types of documents blocking all access to system files
+# ====================================================================================================================||
+
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@||
